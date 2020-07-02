3619fc9b72af36ef8a8fb35f7f88d0baf4db519f
liesly <1163392377@qq.com>
2020-07-02 10:39:46 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/lieslycat/consumer
+
+go 1.14
+
+require (
+	github.com/lieslycat/no_imp v0.0.0
+	github.com/lieslycat/no_interface v0.0.0
+)
---
+++ ma.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+		"github.com/lieslycat/no_interface"
+		"github.com/lieslycat/no_imp"
+)
+
+func test(imp no_imp.Inter) {
+		imp.Interface_func()
+}
+
+func main() {
+		s = no_interface.No_Interface {
+				Name: "xxx",
+		}
+		test(s)
+}
